52b5eb93d49f75f3f6d3620c86706c1aca56849b
bailoo <abhishek.singh.bailoo@gmail.com>
2016-05-11 12:57:12 +0000

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_dp_woocommerce_sessions.sql
+++ wp-content/uploads/revisr-backups/revisr_dp_woocommerce_sessions.sql
@@ -19,11 +19,12 @@ CREATE TABLE `dp_woocommerce_sessions` (
   `session_expiry` bigint(20) NOT NULL,
   PRIMARY KEY (`session_key`),
   UNIQUE KEY `session_id` (`session_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=319 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=356 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `dp_woocommerce_sessions` WRITE;
 /*!40000 ALTER TABLE `dp_woocommerce_sessions` DISABLE KEYS */;
+INSERT INTO `dp_woocommerce_sessions` VALUES (355,'ea9e2e85ee1c84bf3633849191c272ad','a:19:{s:4:\"cart\";s:309:\"a:1:{s:32:\"78679495fe70bfa486d8aaff1a2e4aa9\";a:9:{s:10:\"product_id\";i:5354;s:12:\"variation_id\";i:0;s:9:\"variation\";a:0:{}s:8:\"quantity\";i:1;s:10:\"line_total\";d:7500;s:8:\"line_tax\";i:0;s:13:\"line_subtotal\";i:7500;s:17:\"line_subtotal_tax\";i:0;s:13:\"line_tax_data\";a:2:{s:5:\"total\";a:0:{}s:8:\"subtotal\";a:0:{}}}}\";s:15:\"applied_coupons\";s:6:\"a:0:{}\";s:23:\"coupon_discount_amounts\";s:6:\"a:0:{}\";s:27:\"coupon_discount_tax_amounts\";s:6:\"a:0:{}\";s:21:\"removed_cart_contents\";s:6:\"a:0:{}\";s:19:\"cart_contents_total\";d:7500;s:5:\"total\";i:0;s:8:\"subtotal\";i:7500;s:15:\"subtotal_ex_tax\";i:7500;s:9:\"tax_total\";i:0;s:5:\"taxes\";s:6:\"a:0:{}\";s:14:\"shipping_taxes\";s:6:\"a:0:{}\";s:13:\"discount_cart\";i:0;s:17:\"discount_cart_tax\";i:0;s:14:\"shipping_total\";i:0;s:18:\"shipping_tax_total\";i:0;s:9:\"fee_total\";i:0;s:4:\"fees\";s:6:\"a:0:{}\";s:10:\"wc_notices\";N;}',1463046455);
 /*!40000 ALTER TABLE `dp_woocommerce_sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
