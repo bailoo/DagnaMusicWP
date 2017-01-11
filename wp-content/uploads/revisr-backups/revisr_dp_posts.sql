6644cfb7c601fa107d96437a29b6c5e63ed409e0
bailoo <abhishek.singh.bailoo@gmail.com>
2017-01-11 12:57:15 +0000

    Backed up the database with Revisr.

--- wp-content/uploads/revisr-backups/revisr_dp_posts.sql
+++ wp-content/uploads/revisr-backups/revisr_dp_posts.sql
@@ -41,7 +41,7 @@ CREATE TABLE `dp_posts` (
   KEY `type_status_date` (`post_type`,`post_status`,`post_date`,`ID`),
   KEY `post_parent` (`post_parent`),
   KEY `post_author` (`post_author`)
-) ENGINE=InnoDB AUTO_INCREMENT=5455 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5454 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 LOCK TABLES `dp_posts` WRITE;
